5c1189d612771767828654ca3219037335dd7746
anatoly <tolyan.yalta@yandex.ru>
2022-09-05 20:00:36 +0300

    Change Task_Medium+

--- Task_Medium+/Program.cs
+++ Task_Medium+/Program.cs
@@ -3,35 +3,99 @@
 // Введем понятие «подчисло». «Подчислом» числа N назовем число M, 
 // которое составлено из его цифр, путем отсекания одной или более цифр справа или слева.
 
-Console.WriteLine("Введите натуральное число N (N>100): ");
-int N = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите натуральное число N (N>100): ");
+//int number = Convert.ToInt32(Console.ReadLine());
+int number = 86080;
 
-int count = 0;
 
-int array_length(int number)
+
+int amount_digits(int number)   // Количество цифр в числе
 {
-    int length = 2;
-    int temp = 99;
+    int amount = 1;
+    int temp = 9;
     while(temp < number)
     {
         if (number > temp)
         {
-            length++;
+            amount++;
         }
         temp = temp * 10 + 9;
     }
-    return length;
+    return amount;
 }
 
-int length1 = array_length(N);
-int length2 = Math.Pow(length1);
-int[,] array = new int [length1, length];
-
-int category_numbers = 1;                       // Заполняем массив цифрами из числа
-for(int i = 0; i < length1; i++)
+bool simple_number(int number)
 {
-    if(i = 0) array[i] = number % 10;
-    category_numbers = category_numbers * 10;
-    array[i] = number / category_numbers % 10;
+    bool result = false;
+    for(int i = 2; i < number; i++)    
+    { 
+        if(number % i == 0) break;
+        else if(i == number - 1)
+        {
+            result = true;
+        }
+    }
+    return result;
+}
+
+
+
+int length = amount_digits(number);
+int temp = length;
+for(int i = 1; i < amount_digits(number) - 1; i++)
+{
+    length = length + (temp - i);
+}
+
+
+int[] array = new int [length];
+
+int amount = amount_digits(number);
+
+int amount1 = 1;
+temp = number;
+for(int i = 0; i < length; i++)
+{
+    array[i] = temp % Convert.ToInt32(Math.Pow(10, amount1));
+    temp = temp / 10;
+    //y = Convert.ToInt32(Math.Pow(10, amount1 - 1));
+    if(temp < Convert.ToInt32(Math.Pow(10, amount1 - 1))) 
+    {
+        amount1 += 1;
+        temp = number;
+    }
+    if(amount1 == amount) break;
+}
+
+for(int i = 0; i < length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+for(int i = 0; i < length; i++)
+{
+    for(int j = i + 1; j < length; j++)
+    {
+        if(array[i] == array[j]) array[j] = -1;
+    
+        //{
+            //bool result = simple_number(array[i]);
+        
+            //if(result == true) count += 1;
+        //}
+        //if(j == length - 1) count += 1;
+    }
+    
+}
+
+Console.Write("\nЧисло N " + count);
+
+Console.WriteLine(" ");
+int count1 = 0;
+int count2 = 0;
+for(int i = 0; i < length; i++)
+{
+    Console.Write(array[i] + " ");
+    if(array[i] >= 0) count1 += 1;
 }
 
