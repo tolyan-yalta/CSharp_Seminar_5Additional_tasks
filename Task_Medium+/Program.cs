7f1e346ee5215871a43d34d9795c9eb9cfa351c4
anatoly <tolyan.yalta@yandex.ru>
2022-09-06 14:22:07 +0300

    Changed Task_Medium+. Final version

--- Task_Medium+/Program.cs
+++ Task_Medium+/Program.cs
@@ -3,11 +3,8 @@
 // Введем понятие «подчисло». «Подчислом» числа N назовем число M, 
 // которое составлено из его цифр, путем отсекания одной или более цифр справа или слева.
 
-//Console.WriteLine("Введите натуральное число N (N>100): ");
-//int number = Convert.ToInt32(Console.ReadLine());
-int number = 86080;
-
-
+Console.WriteLine("Введите натуральное число N (N>100): ");
+int number = Convert.ToInt32(Console.ReadLine());
 
 int amount_digits(int number)   // Количество цифр в числе
 {
@@ -24,7 +21,7 @@ int amount_digits(int number)   // Количество цифр в числе
     return amount;
 }
 
-bool simple_number(int number)
+bool simple_number(int number)  // Проверка простое ли число
 {
     bool result = false;
     for(int i = 2; i < number; i++)    
@@ -40,25 +37,22 @@ bool simple_number(int number)
 
 
 
-int length = amount_digits(number);
+int length = amount_digits(number);     // Определяем размер массива
 int temp = length;
 for(int i = 1; i < amount_digits(number) - 1; i++)
 {
     length = length + (temp - i);
 }
 
-
 int[] array = new int [length];
 
 int amount = amount_digits(number);
-
 int amount1 = 1;
 temp = number;
-for(int i = 0; i < length; i++)
+for(int i = 0; i < length; i++)     // Заполняем массив подчислами
 {
     array[i] = temp % Convert.ToInt32(Math.Pow(10, amount1));
     temp = temp / 10;
-    //y = Convert.ToInt32(Math.Pow(10, amount1 - 1));
     if(temp < Convert.ToInt32(Math.Pow(10, amount1 - 1))) 
     {
         amount1 += 1;
@@ -67,35 +61,35 @@ for(int i = 0; i < length; i++)
     if(amount1 == amount) break;
 }
 
-for(int i = 0; i < length; i++)
+for(int i = 0; i < length; i++)     // Для наглядности выводим получившийся массив
 {
     Console.Write(array[i] + " ");
 }
 
-for(int i = 0; i < length; i++)
+for(int i = 0; i < length; i++)             // Проверка чисел на повторы
 {
     for(int j = i + 1; j < length; j++)
     {
         if(array[i] == array[j]) array[j] = -1;
-    
-        //{
-            //bool result = simple_number(array[i]);
-        
-            //if(result == true) count += 1;
-        //}
-        //if(j == length - 1) count += 1;
-    }
-    
+    }  
 }
-
-Console.Write("\nЧисло N " + count);
 
 Console.WriteLine(" ");
-int count1 = 0;
-int count2 = 0;
-for(int i = 0; i < length; i++)
-{
+
+for(int i = 0; i < length; i++)     // Для наглядности выводим получившийся массив 
+{                                   // с повторами замененными на -1
     Console.Write(array[i] + " ");
-    if(array[i] >= 0) count1 += 1;
 }
 
+int count1 = 0;
+int count2 = 0;
+bool result =false;
+for(int i = 0; i < length; i++)     // Подсчет количества подчисел и прстых чисел
+{
+    if(array[i] >= 0) count1 += 1;
+    result = simple_number(array[i]);
+    if(result == true) count2 += 1;
+    result =false;
+}
+
+Console.WriteLine("\nЧисло N имеет " + count1 + " подчисел, из них " + count2 + " являются простыми.");
